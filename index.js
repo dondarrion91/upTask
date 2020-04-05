8da809dc463d516b76fd5bda9265f96e4ea6c25e
Julian <julisanchez1991@gmail.com>
2020-04-05 18:42:38 -0300

    test

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.use((req,res,next) => {
 app.use('/',routes());
 
 
-const host = process.env.BD_HOST || '0.0.0.0';
+const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 app.listen(port,host,() => {
